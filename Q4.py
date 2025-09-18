87bdd74a49328f4c74437adeb07cd09dbabc5c7d
Sura8829 <SURAJGAUTAMOFFICIAL93@GMAIL.COM>
2025-09-18 14:27:30 +0530

    Delete Q4.py

--- Q4.py
+++
@@ -1,7 +0,0 @@
-import keyword
-keywordslist = keyword.kwlist
-a = input("Please enter any key word: ")
-if a in keywordslist:
-        print( ValueError("This is a keyword"))
-else:
-        print("No keyword is wrong ")
